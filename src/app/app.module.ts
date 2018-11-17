99a673dc63b3e958dea27c1b3b8d4d48702645d3
Exponent500 <reuvenkishon@gmail.com>
2018-11-17 13:17:56 -0800

    Re organize App and Core Modules
    1. moved providers array from App to Core Module
    2. Remove unneccessary import of RouterModule within the AppModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,13 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { RouterModule } from '@angular/router';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { CoreModule } from './core/core.module';
-import { CoinsService } from './coins/coins.service';
-import { CryptoCompareDataService } from './shared/cryptocompare/cryptocompare-data.service';
 import { SharedModule } from './shared/shared.module';
 
 @NgModule({
@@ -16,13 +13,11 @@ import { SharedModule } from './shared/shared.module';
   ],
   imports: [
     BrowserModule,
-    RouterModule,
     HttpClientModule,
     CoreModule,
     AppRoutingModule,
     SharedModule
   ],
-  providers: [CoinsService, CryptoCompareDataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { AppRoutingModule } from '../app-routing.module';
+import { CoinsService } from '../coins/coins.service';
+import { CryptoCompareDataService } from '../shared/cryptocompare/cryptocompare-data.service';
 
 @NgModule({
-    declarations: [HomeComponent, HeaderComponent],
+    declarations: [
+        HomeComponent,
+        HeaderComponent
+    ],
     imports: [AppRoutingModule],
-    exports: [HeaderComponent, AppRoutingModule]
+    exports: [
+        HeaderComponent,
+        AppRoutingModule
+    ],
+    providers: [
+        CoinsService,
+        CryptoCompareDataService
+    ]
 })
 export class CoreModule {}
