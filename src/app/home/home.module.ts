9a27627bb179bed480535372bb77e26cc9528b6e
Exponent500 <reuvenkishon@gmail.com>
2018-12-05 15:37:04 -0800

    Update angular material spinner behavior on Coins List and Home Pages

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 import { HomeComponent } from './home.component';
 import { SharedModule } from '../shared/shared.module';
 import { CoinsByTotalVolumeResolverForHomePage } from './home-resolver.service';
+import { MatProgressSpinnerModule } from '@angular/material';
 
 @NgModule({
     declarations: [HomeComponent],
-    imports: [SharedModule],
+    imports: [
+        SharedModule,
+        MatProgressSpinnerModule
+    ],
     exports: [HomeComponent],
     providers: [CoinsByTotalVolumeResolverForHomePage]
 })
