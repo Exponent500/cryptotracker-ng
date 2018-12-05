7a4d0aea30fa0b9f07bf90f1e71431aef79e831e
Exponent500 <reuvenkishon@gmail.com>
2018-12-05 11:35:29 -0800

    Pull out coin table from CoinsComponent and place it into a new CoinTableComponent

---
+++ src/app/coins/coin-table/coin-table.component.ts
@@ -0,0 +1,11 @@
+import { Component, Input } from '@angular/core';
+import { CoinDataWithSocketData } from 'src/app/shared/cryptocompare/interfaces';
+
+@Component({
+    selector: 'app-coin-table',
+    templateUrl: './coin-table.component.html',
+    styleUrls: ['./coin-table.component.scss']
+})
+export class CoinTableComponent {
+    @Input() coinData: CoinDataWithSocketData[];
+}
