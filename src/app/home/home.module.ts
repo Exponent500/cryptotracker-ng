f71aeeecd69ba95e99a4898377f585658e6f5387
Exponent500 <reuvenkishon@gmail.com>
2018-12-05 16:59:22 -0800

    Add semantic HTML and update the coin table footers on both CoinsComponent and HomeComponent

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -4,12 +4,14 @@ import { HomeComponent } from './home.component';
 import { SharedModule } from '../shared/shared.module';
 import { CoinsByTotalVolumeResolverForHomePage } from './home-resolver.service';
 import { MatProgressSpinnerModule } from '@angular/material';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
     declarations: [HomeComponent],
     imports: [
         SharedModule,
-        MatProgressSpinnerModule
+        MatProgressSpinnerModule,
+        RouterModule
     ],
     exports: [HomeComponent],
     providers: [CoinsByTotalVolumeResolverForHomePage]
